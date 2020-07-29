9c5023ac956eb2c5f3e93f03a209d147fbd22c6b
sairamaj <sairamaj@gmail.com>
2020-07-29 11:11:26 -0700

    atoi conversion

---
+++ problems/atoi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int convert(char *data){
+    int len = strlen(data);
+    char *ptr = data + len-1;
+    int i = len;
+    int value = 0;
+    int asciiZero = 48;
+    int place =1;
+    while(i >0 ){
+        int digit = (int)*ptr - asciiZero;
+        value += (digit* place);
+        i--;
+        ptr--;
+        place = place * 10;
+    }
+
+    return value;
+
+}
+void main(){
+    char data[20];
+    printf("value to convert:");
+    scanf("%s", data);
+
+    int val = convert(data);
+    printf("val:%d", val);
+}
