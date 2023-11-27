34dbc7d41819a4b58eef962c16ad60d52c01aa06
sai@pqa.net <sai@pqa.net>
2023-11-27 22:46:31 +0000

    adding patterns

--- coding/misc/fac.cpp
+++ coding/misc/fac.cpp
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+/*
+	curVal = 5
+	  (4,5)
+	  4*5 = 20
+	 (3,20)
+	 (2,60)
+	 (1,120);
+
+	A special form of recursion where the last operation of a function is a recursive call. The recursion may be optimized away by executing the call in the current stack frame and returning its result rather than creating a new stack frame.
+*/
+int facrecbyTail(int i, int curVal)
+{
+	if( i== 1)
+	{
+	  return curVal;
+	}
+
+	return facrecbyTail(i-1, curVal*i);
+}
+
+
 int facrec(int i)
 {
 	if( i== 1)
@@ -25,7 +46,13 @@ void main()
 {
 	printf("%d\n",fac(5));
 	printf("%d\n",facrec	(5));
+	printf("%d\n",facrecbyTail(5,1));
+
+	printf("_______________\n");
+	
 	printf("%d\n",fac(6));
 	printf("%d\n",facrec	(6));
-	
+	printf("%d\n",facrecbyTail(6,1));
+
+	printf("_______________\n");
 }
